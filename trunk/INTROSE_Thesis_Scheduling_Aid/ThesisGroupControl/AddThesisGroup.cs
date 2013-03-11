6724852dfe14df8ffd533b6b25478a5000e19dc8
chessmotif@gmail.com <chessmotif@gmail.com@7896b665-d195-a08b-5b20-52142dec28a0>
2013-03-11 14:52:02 +0000

    temporary (very) rough draft for the thesis group stuff. most UI is done na, yung DB stuff + functions nalang i guess

---
+++ trunk/INTROSE_Thesis_Scheduling_Aid/ThesisGroupControl/AddThesisGroup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ThesisGroupControl
+{
+    public partial class AddThesisGroup : UserControl
+    {
+        public AddThesisGroup()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void init()
+        {
+            thesisAddTextBox.Visible = false;
+            thesisSelectComboBox.Visible = true;
+
+        }
+
+        private void studentBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.studentBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.iNTROSEDBDataSet);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Add A Thesis Group")
+            {
+                label5.Text = button1.Text;
+                button1.Text = "Select A Thesis Group";
+                thesisSelectComboBox.Visible = true;
+                thesisAddTextBox.Visible = false;
+            }
+            else
+            {
+                label5.Text = button1.Text;
+                button1.Text = "Add A Thesis Group";
+                thesisSelectComboBox.Visible = false;
+                thesisAddTextBox.Visible = true;
+            }
+        }
+    }
+}
